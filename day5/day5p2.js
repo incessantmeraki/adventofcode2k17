052d2ea2096d1e1b95323b8ee0ae3c0dc4e2be22
incessantmeraki <incessantmeraki@gmail.com>
2017-12-05 23:18:18 +0545

    day 5 solution

---
+++ day5/day5p2.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+
+var input = fs.readFileSync('./input').toString()
+input = input.trim()
+
+var input = input.split('\n')
+var input = input.map(function (item) {
+  return parseInt(item, 10)
+})
+
+var steps = 0
+var i = 0
+while (i < input.length) {
+  steps++
+  var t = input[i]
+  if (t >= 3)
+    input[i]--
+  else
+    input[i]++
+  i = i + t
+}
+
+console.log('stps ', steps)
